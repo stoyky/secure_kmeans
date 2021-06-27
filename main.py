5f024f44acc7a1e68200ff30ecf91ae9cb19d9c7
stoyky <hello@check.nl>
2021-06-27 11:12:37 +0200

    Preliminary fix for SSP seems to work...

--- main.py
+++ main.py
@@ -7,33 +7,36 @@ if __name__ == '__main__':
     epsilon = 1
     max_iter = 15
 
-    range_start = 100
-    range_end = 300
-    step = 100
+    range_start = 1000
+    range_end = 10000
+    step = 1000
     n_timings = 1
 
     centroids = random_centroids(k)
 
-    timings_sklearn = []
-    for i in range(range_start, range_end, step):
-        print(i)
-        data = gen_data(k, n_samples=i)
-        t = Timer(lambda: KMeans(n_clusters=k, max_iter=max_iter).fit(data))
-        timings_sklearn.append(t.timeit(number=n_timings))
-    print(timings_sklearn)
+    # timings_sklearn = []
+    # for i in range(range_start, range_end, step):
+    #     print(i)
+    #     data = gen_data(k, n_samples=i)
+    #     t = Timer(lambda: KMeans(n_clusters=k, max_iter=max_iter).fit(data))
+    #     timings_sklearn.append(t.timeit(number=n_timings))
+    # print(timings_sklearn)
+    #
+    # timings_naive = []
+    # for i in range(range_start, range_end, step):
+    #     print(i)
+    #     data = gen_data(k, n_samples=i)
+    #     t = Timer(lambda: naive_kmeans(data, centroids, k, epsilon, max_iter, False))
+    #     timings_naive.append(t.timeit(number=n_timings))
+    # print(timings_naive)
 
-    timings_naive = []
-    for i in range(range_start, range_end, step):
-        print(i)
-        data = gen_data(k, n_samples=i)
-        t = Timer(lambda: naive_kmeans(data, centroids, k, epsilon, max_iter, False))
-        timings_naive.append(t.timeit(number=n_timings))
-    print(timings_naive)
+    # timings_secure = []
+    # for i in range(range_start, range_end, step):
+    #     print(i)
+    #     data = gen_data(k, n_samples=i)
+    #     t = Timer(lambda: secure_kmeans(data, centroids, k, epsilon, max_iter, False))
+    #     timings_secure.append(t.timeit(number=n_timings))
+    # print(timings_secure)
 
-    timings_secure = []
-    for i in range(range_start, range_end, step):
-        print(i)
-        data = gen_data(k, n_samples=i)
-        t = Timer(lambda: secure_kmeans(data, centroids, k, epsilon, max_iter, False))
-        timings_secure.append(t.timeit(number=n_timings))
-    print(timings_secure)
+    data = gen_data(k, n_samples=1000)
+    secure_kmeans(data, centroids, k, epsilon, max_iter, True)
--- ssp.py
+++ ssp.py
@@ -8,8 +8,7 @@ def dot(K, L):
     return sum(i[0] * i[1] for i in zip(K, L))
 
 
-def ssp(pubkey, prikey, n, X, Y, mult=1):
-    dot_product = dot(X,Y)
+def ssp(pubkey, prikey, n, X, Y, mult):
 
     X = [int(x)*mult for x in X]
     Y = [int(y) for y in Y]
@@ -20,19 +19,13 @@ def ssp(pubkey, prikey, n, X, Y, mult=1):
     for i in range(N):
       c.append((pubkey.raw_encrypt(X[i], r_value=25)))  #r value must be gcd(r, n) = 1
 
-    w = (c[0] ** Y[0])
-
-    for i in range(1, N):
-        w *= (c[i] ** Y[i])
-
+    w = 0
+    for i in range(0, N):
+        w *= (c[i] ** Y[i]) % n
 
     sb = secrets.randbelow(n)
-    t = pubkey.raw_encrypt(-sb, r_value=3)
-    w = (t * w)
-    sa = prikey.raw_decrypt(int(w))
-
-    #truth = (mult*dot_product - sb) % n
-    #print(" sa: {0} | sb: {1} | 2x dot product: {2} | true sa: {3} | n: {4} ".format(sa, sb, mult*dot_product, truth, n))
+    w *= pubkey.raw_encrypt(-sb, r_value=3)
+    sa = dot(X, Y) - sb
 
     return sa, sb
 

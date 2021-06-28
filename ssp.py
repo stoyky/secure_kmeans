6a55c58245c8fc3f373f9a88dbea1e21d7f43399
Susi <09srai4@gmail.com>
2021-06-28 12:14:33 +0200

    fix for incorrect sa

--- ssp.py
+++ ssp.py
@@ -8,10 +8,11 @@ def dot(K, L):
     return sum(i[0] * i[1] for i in zip(K, L))
 
 
+
 def ssp(pubkey, prikey, n, X, Y, mult):
 
-    X = [int(x)*mult for x in X]
-    Y = [int(y) for y in Y]
+    X = [(int(x)*mult) % n for x in X]
+    Y = [(int(y)) % n for y in Y]
 
     # Alice creates C.
     c = []
@@ -19,12 +20,12 @@ def ssp(pubkey, prikey, n, X, Y, mult):
     for i in range(N):
       c.append((pubkey.raw_encrypt(X[i], r_value=25)))  #r value must be gcd(r, n) = 1
 
-    w = (c[0] ** Y[0]) % n
+    w = (c[0] ** Y[0])
     for i in range(1, N):
-        w *= (c[i] ** Y[i]) % n
+        w *= (c[i] ** Y[i])
 
     sb = secrets.randbelow(n)
     w *= pubkey.raw_encrypt(-sb, r_value=3)
-    sa = dot(X, Y) - sb
+    sa = prikey.raw_decrypt(w)
 
     return sa, sb

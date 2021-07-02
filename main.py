2677c214789aa7a155e618a0130c1b2d4c8bf240
stoyky <hello@check.nl>
2021-07-02 08:31:55 +0200

    Added title and labels to graph plot

--- main.py
+++ main.py
@@ -11,12 +11,16 @@ def graph_performance(sk, naive, secure, range_start, range_end, step):
     """
     x = [i for i in range(range_start, range_end, step)]
 
-    plt.plot(x, sk)
-    plt.plot(x, naive)
     plt.plot(x, secure)
+    plt.plot(x, naive)
+    plt.plot(x, sk)
 
+    plt.title("Comparison of baseline against naive and secure implementation.")
+    plt.xlabel("No. of points in dataset")
+    plt.ylabel("Time (s)")
     plt.yscale('log')
-    plt.legend(['scikit k-means', 'naive k-means', 'secure k-means'])
+
+    plt.legend(['secure k-means', 'naive k-means', 'scikit k-means', ])
     plt.show()
 
 
@@ -42,9 +46,9 @@ if __name__ == '__main__':
     max_iter = 15
 
     range_start = 1000
-    range_end = 32000
+    range_end = 33000
     step = 1000
-    n_timings = 10
+    n_timings = 3
 
     centroids = random_centroids(k)
 
@@ -54,7 +58,7 @@ if __name__ == '__main__':
         data = gen_data(k, n_samples=i)
         t = Timer(lambda: KMeans(n_clusters=k, max_iter=max_iter).fit(data))
         timings_sklearn.append(t.timeit(number=n_timings))
-    print(timings_sklearn)
+        print(timings_sklearn)
 
     timings_naive = []
     for i in range(range_start, range_end, step):
@@ -62,7 +66,7 @@ if __name__ == '__main__':
         data = gen_data(k, n_samples=i)
         t = Timer(lambda: naive_kmeans(data, centroids, k, epsilon, max_iter, False))
         timings_naive.append(t.timeit(number=n_timings))
-    print(timings_naive)
+        print(timings_naive)
 
     timings_secure = []
     for i in range(range_start, range_end, step):
@@ -70,7 +74,7 @@ if __name__ == '__main__':
         data = gen_data(k, n_samples=i)
         t = Timer(lambda: secure_kmeans(data, centroids, k, epsilon, max_iter, False))
         timings_secure.append(t.timeit(number=n_timings))
-    print(timings_secure)
+        print(timings_secure)
 
     print(timings_sklearn)
     print(timings_naive)
